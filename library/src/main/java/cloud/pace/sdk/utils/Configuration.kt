a5629bbe94d83485ea3db9881ff29f8e27061092
Melissa Spinuzza <melissa@pace.car>
2021-02-11 15:01:19 +0100

    Remove initial access token

--- library/src/main/java/cloud/pace/sdk/utils/Configuration.kt
+++ library/src/main/java/cloud/pace/sdk/utils/Configuration.kt
@@ -6,16 +6,11 @@ data class Configuration @JvmOverloads constructor(
     var clientAppBuild: String,
     var apiKey: String,
     var clientId: String? = null,
-    var accessToken: String? = null,
     var authenticationMode: AuthenticationMode = AuthenticationMode.WEB,
     var environment: Environment = Environment.PRODUCTION,
     var extensions: List<String> = emptyList(),
     var locationAccuracy: Int? = null
-) {
-    init {
-        authenticationMode = if (accessToken != null) AuthenticationMode.NATIVE else authenticationMode
-    }
-}
+)
 
 enum class AuthenticationMode(val value: String) {
     NATIVE("Native"),
