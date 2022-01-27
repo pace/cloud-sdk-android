9135ce820a58819e75934e3e3416d7fecb7c9682
Melissa Spinuzza <melissa@pace.car>
2022-01-27 16:11:56 +0100

    Adjust TokenValidator.paymentMethodKinds(accessToken) function

--- library/src/main/java/cloud/pace/sdk/appkit/utils/TokenValidator.kt
+++ library/src/main/java/cloud/pace/sdk/appkit/utils/TokenValidator.kt
@@ -45,19 +45,30 @@ object TokenValidator {
 
     /**
      * Returns a list of payment method kinds that are currently allowed to be onboarded for the passed [accessToken].
+     * If it returns `null`, no payment method kinds are allowed to be onboarded.
+     * If it returns an empty list, all payment method kinds are allowed to be onboarded.
      *
-     * @return A list of payment method kinds e.g. `paypal`, `paydirekt`, `creditcard`
+     * @return A list of payment method kinds (e.g. `paypal`, `paydirekt`, `creditcard`), `null` or an empty list
      */
-    fun paymentMethodKinds(accessToken: String): List<String> {
+    fun paymentMethodKinds(accessToken: String): List<String>? {
         val scopes = scopes(accessToken)
-        val prefix = "payment-method:create:"
+        val paymentMethodCreateScope = "pay:payment-methods:create"
+        val prefix = "$paymentMethodCreateScope:"
 
-        return scopes.mapNotNull {
+        if (scopes.contains(paymentMethodCreateScope)) {
+            // The wildcard scope is included which means that all payment method kinds are supported
+            return emptyList()
+        }
+
+        val kinds = scopes.mapNotNull {
             if (it.startsWith(prefix)) {
                 it.substring(prefix.length)
             } else {
                 null
             }
         }
+
+        // Return null to indicate that no payment method kinds are supported
+        return if (kinds.isEmpty()) null else kinds
     }
 }
