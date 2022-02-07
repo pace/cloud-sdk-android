37f39b1e2136acd582e734d6c4d84aaf77a4ddb7
Melissa Spinuzza <melissa@pace.car>
2022-02-07 16:44:30 +0100

    Revert payment method kind scopes changes

--- library/src/main/java/cloud/pace/sdk/appkit/utils/TokenValidator.kt
+++ library/src/main/java/cloud/pace/sdk/appkit/utils/TokenValidator.kt
@@ -26,49 +26,4 @@ object TokenValidator {
 
         return expirationTime > TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) - TEN_MINUTES_IN_SECONDS
     }
-
-    /**
-     * Returns the scopes of the passed [accessToken].
-     *
-     * @return A list of scopes
-     */
-    fun scopes(accessToken: String): List<String> {
-        val decodedJwtPayload: JSONObject?
-        return try {
-            decodedJwtPayload = JWTUtils.decodeJwtPayload(accessToken)
-            decodedJwtPayload.getString("scope").split(" ")
-        } catch (e: Exception) {
-            Timber.e(e, "Failed to decode token $accessToken or its scopes")
-            return emptyList()
-        }
-    }
-
-    /**
-     * Returns a list of payment method kinds that are currently allowed to be onboarded for the passed [accessToken].
-     * If it returns `null`, no payment method kinds are allowed to be onboarded.
-     * If it returns an empty list, all payment method kinds are allowed to be onboarded.
-     *
-     * @return A list of payment method kinds (e.g. `paypal`, `paydirekt`, `creditcard`), `null` or an empty list
-     */
-    fun paymentMethodKinds(accessToken: String): List<String>? {
-        val scopes = scopes(accessToken)
-        val paymentMethodCreateScope = "pay:payment-methods:create"
-        val prefix = "$paymentMethodCreateScope:"
-
-        if (scopes.contains(paymentMethodCreateScope)) {
-            // The wildcard scope is included which means that all payment method kinds are supported
-            return emptyList()
-        }
-
-        val kinds = scopes.mapNotNull {
-            if (it.startsWith(prefix)) {
-                it.substring(prefix.length)
-            } else {
-                null
-            }
-        }
-
-        // Return null to indicate that no payment method kinds are supported
-        return if (kinds.isEmpty()) null else kinds
-    }
 }
