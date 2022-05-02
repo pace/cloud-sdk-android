498b4f3e7c9ce7b1999813eb9fc0a174d6ed1758
Michael Tran <michael@pace.car>
2022-05-02 18:32:05 +0200

    chore: use cdn instead of cms

--- library/src/main/java/cloud/pace/sdk/poikit/poi/download/CdnAPI.kt
+++ library/src/main/java/cloud/pace/sdk/poikit/poi/download/CdnAPI.kt
@@ -1,6 +1,5 @@
 package cloud.pace.sdk.poikit.poi.download
 
-import cloud.pace.sdk.api.API
 import cloud.pace.sdk.api.utils.EnumConverterFactory
 import cloud.pace.sdk.api.utils.InterceptorUtils
 import okhttp3.OkHttpClient
@@ -10,22 +9,22 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.HeaderMap
 
-object CmsAPI {
+object CdnAPI {
     interface GetPaymentMethodVendorsService {
-        @GET("cms/payment-method-vendors")
+        @GET("pay/payment-method-vendors.json")
         fun getPaymentMethodVendors(@HeaderMap headers: Map<String, String>): Call<List<PaymentMethodVendor>>
     }
 
     private val service: GetPaymentMethodVendorsService by lazy {
         Retrofit.Builder()
             .client(OkHttpClient.Builder().addInterceptor(InterceptorUtils.getInterceptor()).build())
-            .baseUrl(API.baseUrl)
+            .baseUrl("https://cdn.pace.cloud")
             .addConverterFactory(EnumConverterFactory())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(GetPaymentMethodVendorsService::class.java)
     }
 
-    fun CmsAPI.getPaymentMethodVendors(additionalHeaders: Map<String, String>? = null) =
+    fun CdnAPI.getPaymentMethodVendors(additionalHeaders: Map<String, String>? = null) =
         service.getPaymentMethodVendors(InterceptorUtils.getHeaders(true, "application/json", "application/json", additionalHeaders))
 }
