8e5fb7c8a492907a904db927977019c23d455ea0
Melanie Bahremand <melanie@pace.car>
2024-02-09 10:48:06 +0100

    CI adjustments

---
+++ scripts/build_cofu_app_from_configuration.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+ASSET_DIRECTORY=configuration/assets
+DRAWABLE_DIRECTORY=app/src/main/res/drawable
+RES_DIRECTORY=app/src/main/res
+
+# The extension for some files is a wildcard, as several file extensions are allowed
+mv configuration/configuration.json configuration.json
+mv ${ASSET_DIRECTORY}/firebase_config_android.json app/google-services.json
+mv ${ASSET_DIRECTORY}/android_keystore.jks keystore-release.jks
+
+for f in ${ASSET_DIRECTORY}/android_launcher_icon_*
+do
+    extension="${f##*.}"
+    density=${f##*android_launcher_icon_}
+    density=${density%%.*}
+    path=${RES_DIRECTORY}/mipmap-${density}
+    output=${path}/ic_launcher.${extension}
+    mkdir -p $path
+    mv "$f" "$output"
+    echo "Moved launcher icon $f to $output"
+done
+
+for f in ${ASSET_DIRECTORY}/*.htm*
+do
+    filename=$(basename -- "$f")
+    filename=${filename%_*}
+    extension="${f##*.}"
+    language=${f##*_}
+    language=${language%-*}
+    language=${language%.*}
+
+    if [ $language = "en" ]; then
+        path=${RES_DIRECTORY}/raw
+    else
+        path=${RES_DIRECTORY}/raw-${language}
+    fi
+
+    output=${path}/${filename}.${extension}
+    mkdir -p $path
+    mv "$f" "$output"
+    echo "Moved legal document $f to $output"
+done
+
+needed_files=("usage_terms" "privacy_statement" "usage_analysis" "imprint")
+for item in "${needed_files[@]}";
+do
+    legal_file=app/src/main/res/raw/${item}.html
+    if [ ! -e "$legal_file" ]; then
+        echo "Error: File '$legal_file' does not exist in base language."
+        exit 1
+    fi
+done
+echo "All legal files exist in base language."
+
+shopt -s extglob
+onboarding_header=false
+home_header=false
+detail_icon=false
+
+for f in ${ASSET_DIRECTORY}/@(*_header_image_android.*|detail_view_brand_icon_android.*)
+do
+    extension="${f##*.}"
+
+    case "$f" in
+        *onboarding_header_image_android*)
+            filename=ic_onboarding_header
+            onboarding_header=true
+            ;;
+        *list_header_image_android*)
+            filename=ic_home_header
+            home_header=true
+            ;;
+        *fallback_header_image_android*) filename=ic_fallback_header;;
+        *detail_view_brand_icon_android.*)
+            filename=ic_brand_logo
+            detail_icon=true
+            ;;
+        *android_notification_icon*) filename=ic_notification;;
+    esac
+
+    output=${DRAWABLE_DIRECTORY}/${filename}.${extension}
+    rm ${DRAWABLE_DIRECTORY}/${filename}.*
+    mv "$f" "$output"
+    echo "Moved image $f to $output"
+done
+
+echo "Onboarding header exists: $onboarding_header"
+echo "Home header exists: $home_header"
+echo "Detail icon exists: $detail_icon"
+apt-get update && apt-get install -y jq
+jq ". + { \"home_show_custom_header\": $home_header, \"onboarding_show_custom_header\": $onboarding_header, \"detail_screen_show_icon\": $detail_icon }" configuration.json > temp.json && mv temp.json configuration.json
+
+./gradlew clean
+./gradlew app:bundleRelease -PbuildNumber=$BUILD_NUMBER -PversionName=$VERSION_NAME
