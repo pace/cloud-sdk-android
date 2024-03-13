1c3caa14f926d769e40a44e7a4e5cd91c3a46a2d
Melanie Bahremand <melanie@pace.car>
2024-03-13 11:40:02 +0100

    Status bar icon color in dark mode

--- app/src/main/java/car/pace/cofu/MainActivity.kt
+++ app/src/main/java/car/pace/cofu/MainActivity.kt
@@ -1,10 +1,12 @@
 package car.pace.cofu
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricPrompt
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.FragmentActivity
 import car.pace.cofu.features.analytics.Analytics
 import car.pace.cofu.features.analytics.AppOpened
@@ -31,8 +33,16 @@ class MainActivity : AppCompatActivity() {
         // This is needed to draw the content edge-to-edge (behind system bars)
         enableEdgeToEdge()
 
+        // This is needed to prevent white status/navigation bar items when dark mode is used, as they would then not be visible on our white background
+        setLightBars(window)
+
         setContent {
             AppContent()
         }
+    }
+
+    private fun setLightBars(window: Window) {
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = true
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightNavigationBars = true
     }
 }
